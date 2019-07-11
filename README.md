4eccdaa6645ddecb957f9b60e768716018bbacca
Luong Quang Manh <luongquangmanh85@gmail.com>
2019-07-11 19:01:17 +0700

    Redesign the routes

--- README.md
+++ README.md
@@ -17,8 +17,25 @@ $ yarn start # yarn dev for development
 
 ## DOCUMENTATION
 
-| ROUTE  | METHOD | REQ BODY                                | RES BODY               | USE                     |
-| ------ | ------ | --------------------------------------- | ---------------------- | ----------------------- |
-| `/`    | GET    |                                         | `{ iam: '/' }`         |
-| `/api` | GET    |                                         | `{ iam: '/api' }`      |
-| `/api` | POST   | `{ url: string, cssSelectors: object }` | `{ success: boolean }` | Execute a watch session |
+### Routes
+
+#### GET `/`
+
+> Just for testing
+
+##### Response body
+
+- iam: `"/"`
+
+#### POST `/`
+
+> Execute a watch session
+
+##### Request body
+
+- url (string): URL to crawl
+- cssSelectors (object): Mapping from CSS selectors to their types. Currently, only "string" is a valid type.
+
+##### Response body
+
+- success (boolean): Status
--- src/server.js
+++ src/server.js
@@ -1,17 +1,29 @@
 require('dotenv-flow').config()
 
 const fastify = require('fastify')
-const apiRoutes = require('./api/routes')
+const { UrlEntity } = require('@albert-team/spiderman/entities')
 const Scheduler = require('./scheduler')
+const Scraper = require('./scraper')
+const DataProcessor = require('./data-processor')
 
 const loggerLevel = process.env.NODE_ENV !== 'production' ? 'debug' : 'info'
 const server = fastify({ ignoreTrailingSlash: true, logger: { level: loggerLevel } })
 const scheduler = new Scheduler()
 
-server.register(apiRoutes, { prefix: '/api', scheduler })
-
 server.get('/', async () => {
   return { iam: '/' }
+})
+
+server.post('/', async (req) => {
+  const { url, cssSelectors } = req.body
+  const urlEntity = new UrlEntity(url, new Scraper(cssSelectors), new DataProcessor(url))
+  try {
+    await scheduler.scheduleUrlEntity(urlEntity)
+    return { success: true }
+  } catch (err) {
+    req.log.error(err.message)
+    return { success: false }
+  }
 })
 
 const start = async () => {
